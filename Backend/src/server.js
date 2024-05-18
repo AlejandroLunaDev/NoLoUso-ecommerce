9cd8bb5f12a49a56b24b83a484e83fd242e66b99
Alejandro Luna <135073545+AlejandroLunaDev@users.noreply.github.com>
2024-05-18 02:44:04 -0300

    maldito cors

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -33,6 +33,13 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://no-lo-uso-ecommerce.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 
 
