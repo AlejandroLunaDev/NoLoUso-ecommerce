5cf41fda755096767a63e655c354e6f417c65a3d
Alejandro Luna <135073545+AlejandroLunaDev@users.noreply.github.com>
2024-05-18 02:27:45 -0300

    maldito cors

--- Backend/src/configs/socketConfig.js
+++ Backend/src/configs/socketConfig.js
@@ -2,7 +2,7 @@ import { Server } from 'socket.io';
 import socketHandler from "../common/utils/socketHandler.js";
 
 const allowedOrigins = process.env.NODE_ENV === 'production' 
-  ? ["https://no-lo-uso-ecommerce.vercel.app/"]
+  ? ["https://no-lo-uso-ecommerce.vercel.app"]
   : ["http://localhost:5173"];
 
 export default (httpServer) => {
