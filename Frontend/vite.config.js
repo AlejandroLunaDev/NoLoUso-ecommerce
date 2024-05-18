0a0de173520499108a8842f4e22f2dcdb2f5bea3
Alejandro Luna <135073545+AlejandroLunaDev@users.noreply.github.com>
2024-05-18 13:39:37 -0300

    portConfigVIte

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,19 +1,23 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import dotenv from 'dotenv'
 
+// Load environment variables from .env file
+dotenv.config()
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server : {
-    port: 8080
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      // eslint-disable-next-line no-undef
-      '@': path.resolve(__dirname, 'src'),
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+
+  return {
+    server: isProduction ? { port: 8080 } : {},
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
     },
-  },
-  
+  }
 })
