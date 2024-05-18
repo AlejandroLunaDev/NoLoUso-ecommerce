b306be9aacda537f61107ce0dd66b30d568e8e60
Alejandro Luna <135073545+AlejandroLunaDev@users.noreply.github.com>
2024-05-17 23:59:01 -0300

    updateCors

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger("dev"));
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://no-lo-uso-ecommerce.vercel.app/",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
