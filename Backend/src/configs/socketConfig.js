5384453fea8ca87518611c71f0ad1643c95d6d49
Alejandro Luna <135073545+AlejandroLunaDev@users.noreply.github.com>
2024-05-18 02:32:06 -0300

    problema cors

--- Backend/src/configs/socketConfig.js
+++ Backend/src/configs/socketConfig.js
@@ -1,14 +1,12 @@
 import { Server } from 'socket.io';
 import socketHandler from "../common/utils/socketHandler.js";
 
-const allowedOrigins = process.env.NODE_ENV === 'production' 
-  ? ["https://no-lo-uso-ecommerce.vercel.app"]
-  : ["http://localhost:5173"];
+
 
 export default (httpServer) => {
   const io = new Server(httpServer, {
     cors: {
-      origin: allowedOrigins,
+      origin: "https://no-lo-uso-ecommerce.vercel.app",
       methods: ["GET", "POST", "PUT", "DELETE"],
       allowedHeaders: ["Content-Type", "Authorization"],
       credentials: true
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger("dev"));
 app.use(cors({
-  origin: "https://no-lo-uso-ecommerce.vercel.app/",
+  origin: "https://no-lo-uso-ecommerce.vercel.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
