27e2d07a0f17e1a24f58f05c8c365717fd217226
AlejandroLunaDev <alejandrolunadev@gmial.com>
2024-06-27 00:12:51 -0300

    CORS

--- Backend/src/configs/socketConfig.js
+++ Backend/src/configs/socketConfig.js
@@ -6,7 +6,7 @@ import socketHandler from "../common/utils/socketHandler.js";
 export default (httpServer) => {
   const io = new Server(httpServer, {
     cors: {
-      origin: "https://no-lo-uso-ecommerce.vercel.app",
+      origin: "https://silouso.shop",
       methods: ["GET", "POST", "PUT", "DELETE"],
       allowedHeaders: ["Content-Type", "Authorization"],
       credentials: true
