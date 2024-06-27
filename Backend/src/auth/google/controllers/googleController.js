f8cd7bdbaf060cd8bb35c39bfb546a9716a03a2b
AlejandroLunaDev <alejandrolunadev@gmial.com>
2024-06-27 00:45:25 -0300

    google login urlcallback

--- Backend/src/auth/google/controllers/googleController.js
+++ Backend/src/auth/google/controllers/googleController.js
@@ -1,19 +1,19 @@
-    // src/controllers/googleController.js
-    import passport from '../googleAuth.js';
+// src/controllers/googleController.js
+import passport from '../googleAuth.js';
 
-    export const googleAuth = passport.authenticate('google', {
-    scope: ['profile', 'email']
-    });
+export const googleAuth = passport.authenticate('google', {
+  scope: ['profile', 'email']
+});
 
-    export const googleCallback = (req, res, next) => {
-    passport.authenticate('google', { session: false }, (err, user, info) => {
+export const googleCallback = (req, res, next) => {
+  passport.authenticate('google', { session: false }, (err, user, info) => {
     if (err) {
-        return res
+      return res
         .status(500)
         .json({ error: 'Error en la autenticación de Google' });
     }
     if (!user) {
-        return res.status(404).json({ error: 'Usuario no encontrado' });
+      return res.status(404).json({ error: 'Usuario no encontrado' });
     }
 
     const { accessToken, refreshToken } = info;
@@ -21,38 +21,45 @@
     let cookieOptions = {};
 
     if (process.env.NODE_ENV === 'production') {
-        cookieOptions = { sameSite: 'None', secure: true, domain: '.railway.app' };
+      cookieOptions = {
+        sameSite: 'None',
+        secure: true,
+        domain: '.silouso.shop'
+      };
     }
 
     res.cookie('accessToken', accessToken, cookieOptions);
     res.cookie('refreshToken', refreshToken, cookieOptions);
 
     const redirectURL =
-        process.env.NODE_ENV === 'production'
-        ? 'https://no-lo-uso-ecommerce.vercel.app/'
+      process.env.NODE_ENV === 'production'
+        ? 'https://silouso.shop/'
         : 'http://localhost:5173/';
 
     res.redirect(redirectURL);
-    })(req, res, next);
-    };
+  })(req, res, next);
+};
 
-    export const googleRedirect = (req, res) => {
-        const { accessToken, refreshToken } = req.cookies; // Obtener los tokens de las cookies
-    
-        let cookieOptions = {};
-    
-        if (process.env.NODE_ENV === 'production') {
-            cookieOptions = { sameSite: 'None', secure: true, domain: '.railway.app' };
-        }
-    
-        res.cookie('accessToken', accessToken, cookieOptions);
-        res.cookie('refreshToken', refreshToken, cookieOptions);
-    
-        const redirectURL =
-            process.env.NODE_ENV === 'production'
-            ? 'https://no-lo-uso-ecommerce.vercel.app/'
-            : 'http://localhost:5173/';
-    
-        res.redirect(redirectURL);
-    };
-    
+export const googleRedirect = (req, res) => {
+  const { accessToken, refreshToken } = req.cookies; // Obtener los tokens de las cookies
+
+  let cookieOptions = {};
+
+  if (process.env.NODE_ENV === 'production') {
+    cookieOptions = {
+        sameSite: 'None',
+        secure: true,
+        domain: '.silouso.shop'
+      };
+  }
+
+  res.cookie('accessToken', accessToken, cookieOptions);
+  res.cookie('refreshToken', refreshToken, cookieOptions);
+
+  const redirectURL =
+    process.env.NODE_ENV === 'production'
+      ? 'https://silouso.shop/'
+      : 'http://localhost:5173/';
+
+  res.redirect(redirectURL);
+};
